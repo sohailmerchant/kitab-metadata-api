805f0ee2c6e6337fc1c710349a32d278e57d420f
S Merchant <sohailsmerchant@gmail.com>
2021-11-15 20:13:44 +0000

    setting changes

--- kitab/settings.py
+++ kitab/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'django_filters',
-    'rest_flex_fields'
-    "corsheaders"
+    'rest_flex_fields',
+    "corsheaders",
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
