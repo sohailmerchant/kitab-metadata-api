ba43dd831912ebcd86f55e61a8558005ae89cd02
S Merchant <sohailsmerchant@gmail.com>
2022-08-07 18:20:18 +0100

    modified:   kitab/settings.py origin allowed true

--- kitab/settings.py
+++ kitab/settings.py
@@ -85,7 +85,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     
 ]
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:8080",
